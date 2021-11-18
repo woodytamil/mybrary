c7e5338574b554530503caf19d0c17c021383ccd
Tamilselvan Ven <tamil33@gmails.com>
2021-11-18 19:26:44 +0530

    updated

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const Author = require('../modals/author');
 // All authers
 router.get('/',(req,res) => {
     res.render('authors/index')
@@ -8,11 +8,11 @@ router.get('/',(req,res) => {
 
 // New Author
 router.get('/new',(req,res) => {
-    res.render('authors/new')
+    res.render('authors/new',{author: new Author()})
 })
 // Create Author
 router.post('/',(req,res) => {
-    res.send('create')
+    res.send(req.body.name)
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
-
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs')
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
