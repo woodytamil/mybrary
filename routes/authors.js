28842f67550e9f42821b10cdeeb3dd3a7ee71597
Tamilselvan Ven <tamil33@gmails.com>
2021-11-19 15:47:14 +0530

    search authors

--- routes/authors.js
+++ routes/authors.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Author = require('../modals/author');
 // All authers
-router.get('/',(req,res) => {
-    res.render('authors/index')
+router.get('/',async (req,res) => {
+    let searchOption = {}
+    if(req.query.name !=null && req.query !==""){
+        searchOption.name = new RegExp(req.query.name,"i")
+    }
+    try{
+        const authors = await Author.find(searchOption)
+        res.render('authors/index',{
+            authors: authors,
+            searchOptions : req.query
+        })
+    }catch{
+        res.redirect('/')
+    }
+    
 })
 
 // New Author
@@ -11,8 +24,20 @@ router.get('/new',(req,res) => {
     res.render('authors/new',{author: new Author()})
 })
 // Create Author
-router.post('/',(req,res) => {
-    res.send(req.body.name)
+router.post('/', async (req,res) => {
+    const author = new Author({
+        name: req.body.name.trim()
+    })
+    try{
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    }catch{
+        res.render('authors/new',{
+            author: author,
+            errorMessage:"Error creating Author"
+        })
+    }
 })
 
 module.exports = router
